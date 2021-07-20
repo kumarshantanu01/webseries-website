f2be123aa754eaeb8d58961f6e9f9857494d92c0
kumarshantanu01 <shantanuk280@gmail.com>
2021-07-21 01:50:10 +0530

    1st commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,38 @@
+const Sdata = [
+    {
+        imgsrc : "https://wallpapercave.com/wp/wp4056410.jpg",
+        sname :"DARK",
+        title:"A Netflix Original Series",
+        link:"https://www.netflix.com/in/title/80100172"
+    },
+
+    {
+        imgsrc : "https://i.pinimg.com/originals/91/31/c3/9131c3a7dfba0258cbb6007ecf7f61e2.jpg",
+        sname :"PEAKY BLINDERS",
+        title:"A Netflix Original Series",
+        link:"https://www.netflix.com/in/title/80002479"
+    },
+
+    {
+        imgsrc : "https://tv-fanatic-res.cloudinary.com/iu/s--POdzeKVH--/t_full/cs_srgb,f_auto,fl_strip_profile.lossy,q_auto:420/v1443466247/vampire-diaries-poster-the-vampire-diaries.png",
+        sname :"VAMPIRE DIARIES",
+        title:"A Netflix Original Series",
+        link:"https://www.netflix.com/in/title/70143860"
+    },
+
+    {
+        imgsrc : "https://static.toiimg.com/thumb/msid-81759086,imgsize-816682,width-800,height-600,resizemode-75/81759086.jpg",
+        sname :"LUCIFER",
+        title:"A Netflix Original Series",
+        link:"https://www.netflix.com/in/title/80057918"
+    },
+
+    {
+        imgsrc : "https://pbs.twimg.com/media/EIsfpsQXUAIsYcj.jpg",
+        sname :"STRANGER THINGS",
+        title:"A Netflix Original Series",
+        link:"https://www.netflix.com/in/title/80057281"
+    },
+
+];
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Card from "./Cards";
+import "./index.css";
+import Sdata from "./Sdata";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+//function ncard(val){}
+ReactDOM.render( 
+<>
+<h1 className="heading_style">list of top 5 NETFLIX SERIES</h1>
+{Sdata.map((val) => {
+  return(
+    <Card  
+ imgsrc={val.imgsrc}
+ title={val.title}
+ sname={val.sname}
+ link={val.link}
+ />
+  );
+  })}
+</>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
